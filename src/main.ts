b5796e45c76b69e7fbcce9cfda817d28e916bead
FoulTrip <davidvasquezmahecha@gmail.com>
2025-04-05 19:33:43 -0500

    refactor: update server port configuration for better flexibility

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,8 @@ import * as responseTime from "response-time"
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(responseTime());
-  await app.listen(3000);
-  // const port = process.env.PORT || 8080;
-  // await app.listen(port, "0.0.0.0");
+  // await app.listen(3000);
+  const port = process.env.PORT || 8080;
+  await app.listen(port, "0.0.0.0");
 }
 bootstrap();
